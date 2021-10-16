e56c7cc68695f55061863cdffbbe7d7605b3e93f
Alon-M <along@moveo.co.il>
2021-10-17 02:00:42 +0300

    Button component with variants + theme provider not finished

---
+++ src/components/UI/Button/Button.stories.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Meta, Story } from "@storybook/react";
+import Button, { ButtonProps } from "./Button";
+
+export default {
+  component: Button,
+  title: "Components/Button",
+} as Meta;
+
+// args in TS way
+const Template: Story<ButtonProps> = (args) => <Button {...args} />;
+
+export const V1 = Template.bind({});
+V1.args = {
+  children: "sfd",
+  onClick: () => {
+    console.log("works");
+  },
+};
+
+export const V2 = Template.bind({});
+V2.args = {
+  children: "sadfvsdfsdfsdfefdsafsdfsd",
+};
