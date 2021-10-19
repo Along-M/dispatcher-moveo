28792adf9e06c3e359c9ab674328ae7b162c9a94
Alon-M <along@moveo.co.il>
2021-10-19 11:22:33 +0300

    Added all cards related components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,75 @@
-import React from "react";
+import * as React from "react";
 import "./App.css";
 import Button from "./components/UI/Button/Button";
+import StoryCard from "./components/UI/Artical-Card/ArticalCard";
 import { ButtonTypes } from "./components/UI/types";
+import DataCard from "./components/UI/Data-Card/DataCard";
+import ArticalCardList from "./components/UI/Artical-Card-list/ArticalCardList";
 
 function App() {
+  const Data = {
+    status: "ok",
+    totalResults: 6645,
+    articles: [
+      {
+        source: {
+          id: "engadget",
+          name: "Engadget",
+        },
+        author: "Karissa Bell",
+        title: "Twitter will let users send and receive Bitcoin tips",
+        description:
+          "Four months after Twitter first introduced in-app tipping, the company is expanding its “tip jar” feature in a major way. The company is opening up tipping to all its users globally, and for the first time will allow some users to send and receive tips in Bit…",
+        url:
+          "https://www.engadget.com/twitter-opens-tipping-in-bitcoin-170017891.html",
+        urlToImage:
+          "https://blogger.googleusercontent.com/img/a/AVvXsEh20SgNNsDlKyWWmB7XgB5SfFY10M6CqJAq93HwGtssTn2cWz6w9zHPjXf91WwoWr27QeaC4HsGv2NxPOXUdvk6xodUojnw8rUuAkEMY3Qb4ucoVpN3nSyF8JW_xVDWa2aSMEWH387hPsfouSJyClLNburIcDbXIeJamuTHwiSvw4hdNnqeeICcvg1wrQ=w1200-h630-p-k-no-nu",
+        publishedAt: "2021-09-23T17:00:17Z",
+        content:
+          "Four months after Twitter first introduced in-app tipping, the company is expanding its tip jar feature in a major way. The company is opening up tipping to all its users globally, and for the first … [+2390 chars]",
+      },
+      {
+        source: {
+          id: "engadget",
+          name: "Engadget",
+        },
+        author: "Andrew Tarantola",
+        title:
+          "Hitting the Books: How Bitcoin is somehow worth more than the paper it's printed on",
+        description:
+          "Bitcoin and similar blockchain-based cryptos exhibit the same radical divergence from traditional scarcity economics that we first saw when MP3s and Napster cratered physical album sales at the turn of the century. Unlike gold, which derives its value from bo…",
+        url:
+          "https://www.engadget.com/hitting-the-books-the-future-of-money-eswar-prasad-harvard-university-press-153024975.html",
+        urlToImage:
+          "https://s.yimg.com/os/creatr-uploaded-images/2021-09/5ea7d740-17ff-11ec-9ffd-ff33ac942d83",
+        publishedAt: "2021-09-25T15:30:24Z",
+        content:
+          "Bitcoin and similar blockchain-based cryptos exhibit the same radical divergence from traditional scarcity economics that we first saw when MP3s and Napster cratered physical album sales at the turn … [+8436 chars]",
+      },
+    ],
+  };
+
+  // const Cards = Data.articles.map((artical, index) => {
+  //   return (
+  //     <StoryCard
+  //       title={artical.title}
+  //       subTitle={"walla.com"}
+  //       tagContent={artical.source.id}
+  //       cardContent={artical.content}
+  //       imgUrl={artical.urlToImage}
+  //     ></StoryCard>
+  //   );
+  // });
+
   return (
-    <Button variant={ButtonTypes.PRIMARY} withIcon={true}>
-      test children
-    </Button>
+    <>
+      {/* <Button variant={ButtonTypes.PRIMARY} withIcon={true}>
+        test children
+      </Button> */}
+      {/* {Cards} */}
+      {/* <DataCard title={"Sources"}></DataCard> */}
+      <ArticalCardList data={Data} />
+    </>
   );
 }
 
--- src/components/UI/Button/style.ts
+++ src/components/UI/Button/style.ts
@@ -4,12 +4,11 @@ import styled from "styled-components";
 
 export const StyledBtn = styled.button`
   border-radius: 20px;
+  width: 242px;
   display: flex;
   flex-direction: row;
-  justify-content: center; 
+  justify-content: space-around; 
   align-items: center;
-  padding: 0px 16px;
-  position: absolute;
   border: none; 
   height: 36px;
   font-family: Roboto;
@@ -18,17 +17,16 @@ export const StyledBtn = styled.button`
   font-size: 14px;
   line-height: 26px;
   color: white;
-  position: absolute;
-  width: 118px; 
   height: 36px;
   left: 8px;
   top: 79px;
   background: #0058B9;
+  align-self: flex-end;
   &:hover {
     opacity: 0.8;
   }
   &.welcome-btn {
-    width: 750px;
+    width: 100%;
     border-radius: 10px;
   }
   &.secondary-gray-btn {
@@ -46,46 +44,3 @@ export const StyledBtn = styled.button`
   }
 }
 `;
-
-// export const PrimaryButton = styled(StyledButton)`
-//   color: white;
-//   position: absolute;
-//   width: 118px; 
-//   height: 36px;
-//   left: 8px;
-//   top: 79px;
-//   background: #0058B9;
-//   &:hover {
-//     opacity: 0.8;
-//   }
-// `
-
-
-// export const WelcomeButton = styled(PrimaryButton)`
-//   width: 780px;
-//   border-radius: 0;
-//   &:hover {
-//     opacity: 0.8;
-//   }
-//   /* width: ${(props:any):any => props.width ? props.width + 'px' : '118px'}; */
-// `
-
-// export const SeconderyButton = styled(StyledBtn)`
-//   background: #D9DBE9;
-//   height: 36px;
-//   color: #5A5A89;
-//   &:hover {
-//     opacity: 0.8;
-//   }
-//   width: ${(props:any):any => props.width ? props.width + 'px' : '115px'};
-// `;
-
-// export const TextButton = styled(StyledBtn)`
-//   background: none;
-//   height: 36px;
-//   color: #5A5A89;
-//   &:hover {
-//     background: rgba(217, 219, 233, 0.3);
-//   }
-//   width: ${(props:any):any => props.width ? props.width + 'px' : '115px'};
-// `;
